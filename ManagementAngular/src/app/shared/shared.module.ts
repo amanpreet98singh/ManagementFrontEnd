caf5415d0ce672a8a3be78e7f60adca653b275ad
Lepo00 <luca.leporati@outlook.com>
2021-02-05 16:55:41 +0100

    shared module ok

--- ManagementAngular/src/app/shared/shared.module.ts
+++ ManagementAngular/src/app/shared/shared.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
+import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [MenuComponent, FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
+  exports:[
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MenuComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
