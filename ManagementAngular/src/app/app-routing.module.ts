38e33ec27c014657b281b01a2e45ea769b5e0859
Lepo00 <luca.leporati@outlook.com>
2021-02-05 17:15:04 +0100

    fix guards

--- ManagementAngular/src/app/app-routing.module.ts
+++ ManagementAngular/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
+import { PagesGuard } from './core/guards/pages.guard';
 
 const routes: Routes = [
-  { path: 'core', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) },
-  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
-  { path: 'invoices', loadChildren: () => import('./features/invoices/invoices.module').then(m => m.InvoicesModule) },
-  { path: 'customers', loadChildren: () => import('./features/customers/customers.module').then(m => m.CustomersModule) },
-  { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) }
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule), canActivate: [AuthGuard],canLoad: [AuthGuard] },
+  { path: 'invoices', loadChildren: () => import('./features/invoices/invoices.module').then(m => m.InvoicesModule), canActivate: [AuthGuard],canLoad: [AuthGuard]},
+  { path: 'customers', loadChildren: () => import('./features/customers/customers.module').then(m => m.CustomersModule), canActivate: [AuthGuard],canLoad: [AuthGuard]},
+  { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule), canActivate: [PagesGuard],canLoad: [PagesGuard] }
 ];
 
 @NgModule({
