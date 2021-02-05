2f3ea4912e3156296faf3037c2756f591007dc94
Lepo00 <luca.leporati@outlook.com>
2021-02-05 17:08:03 +0100

    bug fixes

--- ManagementAngular/src/app/app-routing.module.ts
+++ ManagementAngular/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [{ path: 'MyFeature', loadChildren: () => import('./my-feature/my-feature.module').then(m => m.MyFeatureModule) }, { path: 'core', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) }, { path: 'menu', loadChildren: () => import('./shared/menu/menu.module').then(m => m.MenuModule) }, { path: 'MyFeature', loadChildren: () => import('./features/my-feature/my-feature.module').then(m => m.MyFeatureModule) }, { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }, { path: 'invoices', loadChildren: () => import('./features/invoices/invoices.module').then(m => m.InvoicesModule) }, { path: 'customers', loadChildren: () => import('./features/customers/customers.module').then(m => m.CustomersModule) }, { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) }];
+const routes: Routes = [
+  { path: 'core', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) },
+  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
+  { path: 'invoices', loadChildren: () => import('./features/invoices/invoices.module').then(m => m.InvoicesModule) },
+  { path: 'customers', loadChildren: () => import('./features/customers/customers.module').then(m => m.CustomersModule) },
+  { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
