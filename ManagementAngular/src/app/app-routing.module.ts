ac8cb0788038c94e194c42008a2ca436206e62bc
Lepo00 <luca.leporati@outlook.com>
2021-02-05 16:51:56 +0100

    menu footer & lazy loading components

--- ManagementAngular/src/app/app-routing.module.ts
+++ ManagementAngular/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'MyFeature', loadChildren: () => import('./my-feature/my-feature.module').then(m => m.MyFeatureModule) }, { path: 'core', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) }, { path: 'menu', loadChildren: () => import('./shared/menu/menu.module').then(m => m.MenuModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
