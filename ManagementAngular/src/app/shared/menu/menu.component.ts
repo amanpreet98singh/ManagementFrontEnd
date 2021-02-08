0f1d33a1434309a8ccb234d966869eebe699d0d7
Lepo00 <luca.leporati@outlook.com>
2021-02-08 18:16:18 +0100

    fix header

--- ManagementAngular/src/app/shared/menu/menu.component.ts
+++ ManagementAngular/src/app/shared/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-  search: boolean;
+  @Input()
+  search:boolean;
+
   constructor() { }
 
   ngOnInit(): void {
-    this.search=true;
   }
 
   logout(): void{
