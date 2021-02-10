070bb05ddd5917b0eb8ecebc8266f1c97d3471c7
Lepo00 <luca.leporati@outlook.com>
2021-02-10 10:06:02 +0100

    fix app-routing

--- ManagementAngular/src/app/app-routing.module.ts
+++ ManagementAngular/src/app/app-routing.module.ts
@@ -8,8 +8,7 @@ const routes: Routes = [
   { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule), canActivate: [AuthGuard],canLoad: [AuthGuard] },
   { path: 'invoices', loadChildren: () => import('./features/invoices/invoices.module').then(m => m.InvoicesModule), canActivate: [AuthGuard],canLoad: [AuthGuard]},
   { path: 'customers', loadChildren: () => import('./features/customers/customers.module').then(m => m.CustomersModule), canActivate: [AuthGuard],canLoad: [AuthGuard]},
-  { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule), canActivate: [PagesGuard],canLoad: [PagesGuard] },
-  { path: 'invoices-create', loadChildren: () => import('./features/invoices-create/invoices-create.module').then(m => m.InvoicesCreateModule) }
+  { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule), canActivate: [PagesGuard],canLoad: [PagesGuard] }
 ];
 
 @NgModule({
